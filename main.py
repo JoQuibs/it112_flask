6dc735898cf2a484e16dbf1e1d5548d42140dc65
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-01-17 05:51:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,22 @@ def about():
     return 'I am a student under the program of IT Web Development, I plan to continue my bachelor degree under the program of Software Development at North Seattle College.'
 
 
+@app.route('/fortune')
+def fortune():
+    return 'Fortune Teller'
+
+
+@app.route('/user/<username>')
+def user_info(username):
+    return
+    render_template('user.html',
+                    user=[{
+                        "name": "Jomar",
+                        "since": 2012
+                    }, {
+                        "name": "Quiban",
+                        "since": 2015
+                    }])
+
+
 app.run(host='0.0.0.0', port=81)
